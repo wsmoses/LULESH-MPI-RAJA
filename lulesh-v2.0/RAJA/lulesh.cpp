65cebca935cfd39eb7831e128574e83b38a222b7
Ubuntu <ubuntu@ip-172-31-14-11.ec2.internal>
2022-03-07 00:10:38 +0000

    slowly kill mpi

--- lulesh-v2.0/RAJA/lulesh.cpp
+++ lulesh-v2.0/RAJA/lulesh.cpp
@@ -2218,33 +2218,7 @@ void LagrangeLeapFrog(Domain* domain)
    /* calculate element quantities (i.e. velocity gradient & q), and update
     * material states */
    //LagrangeElements(domain, domain->numElem());
-
-#if USE_MPI   
-#if defined(SEDOV_SYNC_POS_VEL_LATE)
-   CommRecv(*domain, MSG_SYNC_POS_VEL, 6,
-            domain->sizeX() + 1, domain->sizeY() + 1, domain->sizeZ() + 1,
-            false, false) ; 
-
-   fieldData[0] = &Domain::x ;
-   fieldData[1] = &Domain::y ;
-   fieldData[2] = &Domain::z ;
-   fieldData[3] = &Domain::xd ;
-   fieldData[4] = &Domain::yd ;
-   fieldData[5] = &Domain::zd ;
-   
-   CommSend(*domain, MSG_SYNC_POS_VEL, 6, fieldData,
-            domain->sizeX() + 1, domain->sizeY() + 1, domain->sizeZ() + 1,
-            false, false) ;
-#endif
-#endif   
-
    domain->dtcourant() = 0;
-
-#if USE_MPI   
-#if defined(SEDOV_SYNC_POS_VEL_LATE)
-   CommSyncPosVel(*domain) ;
-#endif
-#endif   
 }
 
 
