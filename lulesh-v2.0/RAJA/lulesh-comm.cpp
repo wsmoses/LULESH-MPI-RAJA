959fd2d146f3a543ec7e4096b3a414806c45262c
Ubuntu <ubuntu@ip-172-31-14-11.ec2.internal>
2022-03-07 06:46:32 +0000

    fd

--- lulesh-v2.0/RAJA/lulesh-comm.cpp
+++ lulesh-v2.0/RAJA/lulesh-comm.cpp
@@ -82,9 +82,9 @@ void CommRecv(Domain& domain, int msgType, Index_t xferFields0,
                 recvCount, baseType, fromRank, msgType,
                 MPI_COMM_WORLD, &status) ;
          Real_t *srcAddr = &domain.commDataRecv[0];
-	 for (Index_t fi=0 ; fi<4; ++fi) {
             Domain_member dest = fieldData[0] ;
             auto dat = &(domain.*dest)(0);
+	 for (Index_t fi=0 ; fi<4; ++fi) {
 	    for (Index_t i=0; i<dz; ++i) {
                for (Index_t j=0; j<dy; ++j) {
                   dat[(dx - 1 + i*dx*dy + j*dx)] = srcAddr[i*dy + j] ;
